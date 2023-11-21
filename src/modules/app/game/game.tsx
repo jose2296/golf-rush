1ab5575efe0a399b759a2cb4dc05c3678876ea00
Jose Jerez <josejerez2296@gmail.com>
2023-11-21 22:26:09 +0000

    chore: add react-three-fiber

--- src/modules/app/game/game.tsx
+++ src/modules/app/game/game.tsx
@@ -1,8 +1,43 @@
-const Game = () => {
+import { KeyboardControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Physics } from "@react-three/rapier";
+import { useMemo, Suspense } from "react";
+import { Experience } from "./experience";
 
-    return (
-        <h1>Game</h1>
-    );
+export const Controls = {
+    forward: "forward",
+    back: "back",
+    left: "left",
+    right: "right",
+    jump: "jump",
+}; 
+
+const Game = () => {
+    const map = useMemo(
+        () => [
+          { name: Controls.forward, keys: ["ArrowUp", "KeyW"] },
+          { name: Controls.back, keys: ["ArrowDown", "KeyS"] },
+          { name: Controls.left, keys: ["ArrowLeft", "KeyA"] },
+          { name: Controls.right, keys: ["ArrowRight", "KeyD"] },
+          { name: Controls.jump, keys: ["Space"] },
+        ],
+        []
+      );
+    
+      return (
+        <div className="w-full h-[100vh]">
+            <KeyboardControls map={map}>
+            <Canvas shadows camera={{ position: [10, 10, 10], fov: 30 }}>
+                <color attach="background" args={["#ececec"]} />
+                <Suspense>
+                <Physics debug>
+                    <Experience />
+                </Physics>
+                </Suspense>
+            </Canvas>
+            </KeyboardControls>
+        </div>
+      );
 };
 
 export default Game;
